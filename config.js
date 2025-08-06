da8c89f382b210f98dc261c3b31b5549e10afe63
milkcoffeemilk <milk.coffee.milk@gmail.com>
2025-08-06 15:36:40 +0800

    123

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const APP_CONFIG = {
     // 請替換為您的統一 Google Apps Script 專案的部署網址
-    gasScriptURL: "https://script.google.com/macros/s/AKfycbw6DYeSnsKgY5qgvhK_5mDaJJ6L4Pe14rbncSuLYz2SKanNoGlfDDGxkkcSxmT3ADh_Mg/exec" 
+    gasScriptURL: "https://script.google.com/macros/s/AKfycbwA4XsoFhEe-LeaSsvXDLArH2fPzID_fovObHb7Waed-x9fnAfnBDZWPZkvSC0-0rcb/exec" 
 };
