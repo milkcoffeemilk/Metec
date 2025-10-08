532ab574004482f58f38ee879d20ef7ebead4081
milkcoffeemilk <milk.coffee.milk@gmail.com>
2025-10-08 15:23:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// config.js
+// 這個檔案包含所有 LIFF 頁面共用的配置資訊。
+
+const APP_CONFIG = {
+    // 請替換為您的統一 Google Apps Script 專案的部署網址
+    gasScriptURL: "https://script.google.com/macros/s/AKfycbwA4XsoFhEe-LeaSsvXDLArH2fPzID_fovObHb7Waed-x9fnAfnBDZWPZkvSC0-0rcb/exec" 
+};
